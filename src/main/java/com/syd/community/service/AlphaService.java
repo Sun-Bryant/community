4c7442bf72167bb5cb1a54f49e283d256b4d7279
syd <syd199708@126.com>
2019-09-08 10:46:35 +0800

    开发事务管理Demo

--- src/main/java/com/syd/community/service/AlphaService.java
+++ src/main/java/com/syd/community/service/AlphaService.java
@@ -1,12 +1,25 @@
 package com.syd.community.service;
 
 import com.syd.community.dao.AlphaDao;
+import com.syd.community.dao.DiscussPostMapper;
+import com.syd.community.dao.UserMapper;
+import com.syd.community.entity.DiscussPost;
+import com.syd.community.entity.User;
+import com.syd.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype") //这是正常的原型模式   默认是singleton单例模式
@@ -14,6 +27,15 @@ import javax.annotation.PreDestroy;
 
     @Autowired
     private AlphaDao alphaDao;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public String find() {
         return alphaDao.select();
@@ -32,6 +54,67 @@ import javax.annotation.PreDestroy;
     @PreDestroy  //在销毁对象之前调用
     public void destroy() {
         System.out.println("销毁AlphaService");
+    }
+
+    // REQUIRED: 支持当前事务(外部事务),如果不存在则创建新事务.
+    // REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
+    // NESTED: 如果当前存在事务(外部事务),则嵌套在该事务中执行(独立的提交和回滚),否则就会REQUIRED一样.
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        // 新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://images.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello");
+        post.setContent("新人报道!");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        Integer.valueOf("abc");
+
+        return "ok";
+    }
+
+    //编程式事务
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://images.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好");
+                post.setContent("我是新人!");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
     }
 
 }
