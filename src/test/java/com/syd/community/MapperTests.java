452cde983fb3468524884492a29e8deec6bf94d9
syd <syd199708@126.com>
2019-09-11 16:36:11 +0800

    开发私信列表功能

--- src/test/java/com/syd/community/MapperTests.java
+++ src/test/java/com/syd/community/MapperTests.java
@@ -3,9 +3,11 @@ package com.syd.community;
 
 import com.syd.community.dao.DiscussPostMapper;
 import com.syd.community.dao.LoginTicketMapper;
+import com.syd.community.dao.MessageMapper;
 import com.syd.community.dao.UserMapper;
 import com.syd.community.entity.DiscussPost;
 import com.syd.community.entity.LoginTicket;
+import com.syd.community.entity.Message;
 import com.syd.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,6 +32,9 @@ public class MapperTests {
 
     @Autowired
     private LoginTicketMapper loginTicketMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testInsertLoginTicket() {
@@ -104,5 +109,27 @@ public class MapperTests {
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testSelectLetters() {
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list) {
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
     }
 }
