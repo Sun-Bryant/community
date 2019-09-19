034609dba1c7a39b67b553fb965da057ed81c003
syd <syd199708@126.com>
2019-09-19 16:38:09 +0800

    Spring整合Elasticsearch

--- src/main/java/com/syd/community/CommunityApplication.java
+++ src/main/java/com/syd/community/CommunityApplication.java
@@ -3,6 +3,8 @@ package com.syd.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 /**
  *
  * 项目启动以CommunityApplication类为入口，其实CommunityApplication类是一个配置类
@@ -10,6 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class CommunityApplication {
+
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突问题
+		// see Netty4Utils.setAvailableProcessors()
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
